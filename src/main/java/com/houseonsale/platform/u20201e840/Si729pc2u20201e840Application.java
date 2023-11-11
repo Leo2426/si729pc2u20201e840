3bde1cb07fe789b3a230462897b4e8377aafb7c4
Leonardo Taype Fernandez <93153931+Leo2426@users.noreply.github.com>
2023-11-10 19:19:12 -0500

    chore:inital commit

---
+++ src/main/java/com/houseonsale/platform/u20201e840/Si729pc2u20201e840Application.java
@@ -0,0 +1,13 @@
+package com.houseonsale.platform.u20201e840;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Si729pc2u20201e840Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Si729pc2u20201e840Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/houseonsale/platform/u20201e840/Si729pc2u20201e840ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.houseonsale.platform.u20201e840;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Si729pc2u20201e840ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
